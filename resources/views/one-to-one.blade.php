97af6a15cc24efc2ab49a844cea2cfe0be0f144b
Hatim Hussein <91913484+HatemFurqan@users.noreply.github.com>
2021-12-19 22:21:58 +0200

    Sorry, one-to-one subscriptions are allowed for memorization
    Sorry, one-to-one subscriptions are allowed for memorization

--- resources/views/one-to-one.blade.php
+++ resources/views/one-to-one.blade.php
@@ -775,7 +775,7 @@ Students from other trajectories shall be able to .enroll in the second phase of
 
     <!-- add frames script -->
 <script src="https://cdn.checkout.com/js/framesv2.min.js"></script>
-<script src="{{ asset('app.js') }}?v=63.1"></script>
+<script src="{{ asset('app.js') }}?v=63.61"></script>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
